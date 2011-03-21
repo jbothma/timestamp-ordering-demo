8cb0960dd420c307f41d20e1bc937c212f27e52e
JD Bothma <jbothma@gmail.com>
2011-03-21 22:46:50 +0000

    Update user list with new transaction number when transaction starts and nil when it ends

--- server.erl
+++ server.erl
@@ -30,7 +30,7 @@ initialize() ->
 %%%%%%%%%%%%%%%%%%%%%%% ACTIVE SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% - The server maintains a list of all connected clients and a store holding
 %% the values of the global variable a, b, c and d 
-%% TrnCnt is Transaction Count - next new transaction TS will be TrnCnt + 1
+%% TrnCnt is Transaction Count, incremented in start_transaction
 server_loop(ClientList, StorePid, TrnCnt) ->
     io:format("ClientList: ~p~n", [ClientList]),
     receive
@@ -45,13 +45,15 @@ server_loop(ClientList, StorePid, TrnCnt) ->
 	    server_loop(remove_client(Client,ClientList), StorePid, TrnCnt);
 	{request, Client} -> 
 	    Client ! {proceed, self()},
-        NewTrnCnt = TrnCnt + 1,
-        io:format("request from ~p, starting transaction ~p~n", [Client, NewTrnCnt]),
-	    server_loop(ClientList, StorePid, NewTrnCnt);
+        io:format("request from ~p, starting new transaction.~n", [Client]),
+        {NewTrnCnt, NewClientList} = start_transaction(TrnCnt, ClientList, Client),
+        io:format("New transaction count stands at ~p~n", [NewTrnCnt]),
+	    server_loop(NewClientList, StorePid, NewTrnCnt);
 	{confirm, Client} -> 
         io:format("confirm from ~p~n", [Client]),
 	    Client ! {abort, self()},
-	    server_loop(ClientList, StorePid, TrnCnt);
+        NewClientList = end_transaction(ClientList, Client),
+	    server_loop(NewClientList, StorePid, TrnCnt);
 	{action, Client, Act} ->
 	    io:format("Received ~p from client ~p.~n", [Act, Client]),
 	    server_loop(ClientList, StorePid, TrnCnt)
@@ -71,20 +73,39 @@ store_loop(ServerPid, Database) ->
     end.
 %%%%%%%%%%%%%%%%%%%%%%% ACTIVE SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Return new client list where the client tuple has been replaced with
+% one where all values are the same except for the new transaction number.
+% !!!NOTE!!! it assumes each client only ever attempts one transaction at a time!
+start_transaction(TrnCnt, ClientList, Client) ->
+    NewTrnCnt           = TrnCnt + 1,
+    [_, _, DEP, OLD]    = find_client(Client, ClientList),
+    TempClientList      = remove_client(Client, ClientList),
+    NewClientData       = [Client, NewTrnCnt, DEP, OLD],
+    NewClientList       = TempClientList ++ [NewClientData], % add updated client data to client list
+    {NewTrnCnt, NewClientList}.
 
+end_transaction(ClientList, Client) ->
+    [_, _, DEP, OLD]    = find_client(Client, ClientList),
+    TempClientList      = remove_client(Client, ClientList),
+    NewClientData       = [Client, nil, DEP, OLD],
+    TempClientList ++ [NewClientData]. % add updated client data to client list
 
 %% - Low level function to handle lists
 add_client(C,ClientList) -> [new_client(C)|ClientList].
 
 new_client(C) -> [C, nil, [],[]].
 
-% remove anything form empty list returns empty list
+% remove anything from empty list returns empty list
 remove_client(_,[]) -> [];
 % if client is first, return rest
 remove_client(C, [ [C|_] | T ]) -> T;
 % falling through when client wasn't first, 
 % recurse down and return first and rest with client removed from in-between
 remove_client(C, [H|T]) -> [H|remove_client(C,T)].
+
+find_client(C, [ [C,TS,DEP,OLD] ]) -> [C,TS,DEP,OLD];
+find_client(C, [ [C,TS,DEP,OLD] | _ ]) -> [C,TS,DEP,OLD];
+find_client(C, [_|T]) -> find_client(C,T).
 
 all_gone([]) -> true;
 all_gone(_) -> false.
