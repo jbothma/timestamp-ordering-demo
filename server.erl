764a83a7754c09c02e18d27d23cb41fc2cdff020
JD Bothma <jbothma@gmail.com>
2011-03-23 13:12:53 +0000

    Implemented rollback upon abort. Not checked much yet.

--- server.erl
+++ server.erl
@@ -57,7 +57,7 @@ server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking) ->
         ServerPid = self(),
         io:format("confirm from ~p~n", [Client]),
         {_, TS, DEP, _} = lists:keyfind(Client, 1, ClientList),
-        CheckingPid = spawn(fun() -> do_commit_loop(TS, DEP, TrnHist, ServerPid) end),
+        CheckingPid = spawn(fun() -> check_commit_loop(TS, DEP, TrnHist, ServerPid) end),
         NewChecking = [CheckingPid|Checking],
 	    server_loop(ClientList, StorePid, TrnCnt, TrnHist, NewChecking);
 	{action, Client, Act} ->
@@ -78,11 +78,16 @@ server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking) ->
 	    server_loop(NewClientList, StorePid, TrnCnt, TrnHist, Checking);
     {abort, TS, ChkPid} ->
         io:format("Server: check said ABORT~n"),
+
+        {Client, _, _, OLD} = lists:keyfind(TS, 2, ClientList),
+
+        NewClientList = rollback(ClientList, OLD, StorePid),
+
+
         %set status in history to aborted
-        NewTrnHist  = lists:keyreplace(TS, 1, TrnHist, {TS, aborted}),
-        NewChecking = Checking--[ChkPid],
-        Client      = element(1, lists:keyfind(TS, 2, ClientList)),
-        NewClientList = end_transaction(ClientList, Client),
+        NewTrnHist      = lists:keyreplace(TS, 1, TrnHist, {TS, aborted}),
+        NewChecking     = Checking--[ChkPid],
+        NewClientList   = end_transaction(ClientList, Client),
 	    Client ! {abort, self()},
         announce_completion(Checking),
         server_loop(NewClientList, StorePid, TrnCnt, NewTrnHist, NewChecking);
@@ -123,7 +128,7 @@ store_loop(ServerPid, Database) ->
                 %do write
                 NewDatabase = do_write(Database, Idx, Val, TS)
         end,
-	    io:format("Database status:~n~p.~n",[Database]),
+	    io:format("Database status:~n~p.~n",[NewDatabase]),
 	    store_loop(ServerPid,NewDatabase);
     {{read, Idx}, TS, ServerPid} ->
         io:format("DB: Must read idx ~p for trn ~p~n", [Idx, TS]),
@@ -138,7 +143,13 @@ store_loop(ServerPid, Database) ->
                 NewDatabase = set_RTS(Database, Idx, erlang:max(RTS, TS))
         end,
 	    io:format("Database status:~n~p.~n",[NewDatabase]),
-	    store_loop(ServerPid, NewDatabase)
+	    store_loop(ServerPid, NewDatabase);
+    {rollback, TS, OLD, ServerPid} ->
+        io:format("DB: rolling back OLD=~p~n", [OLD]),
+        NewDatabase = rollback(Database, TS, OLD),
+        ServerPid ! rollback_complete,  % server blocks until this is done but not sure it's necesary TODO
+	    io:format("Database status:~n~p.~n",[NewDatabase]),
+        store_loop(ServerPid, NewDatabase)
     end.
 
 % spawned as separate process from server_loop.
@@ -146,15 +157,15 @@ store_loop(ServerPid, Database) ->
 % Wait for a 'someone_completed' from server_loop each 
 % time a running transaction is encountered in DEP.
 % restart loop with up-to-date history upon 'someone_completed'
-do_commit_loop(TS, DEP, TrnHist, ServerPid) ->
+check_commit_loop(TS, DEP, TrnHist, ServerPid) ->
     %io:format("checking trn ~p: DEP=~p   TrnHist=~p~n", [TS, sets:to_list(DEP), TrnHist]),
-    CommitCheck = check_history(TrnHist, sets:to_list(DEP)),
+    CommitCheck = check_history(TS, TrnHist, sets:to_list(DEP)),
     case CommitCheck of
         wait ->
             io:format("checking: wait~n"),
             receive 
             {someone_completed, NewTrnHist} ->
-                do_commit_loop(TS, DEP, NewTrnHist, ServerPid)
+                check_commit_loop(TS, DEP, NewTrnHist, ServerPid)
             end;
         abort ->
             %io:format("checking: abort~n"),
@@ -165,6 +176,23 @@ do_commit_loop(TS, DEP, TrnHist, ServerPid) ->
     end.
 
 %%%%%%%%%%%%%%%%%%%%%%% ACTIVE SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%        for each (oldO_j, oldWTS(O_j)) in OLD(T_i):     // make sure nothing persists from this aborting transaction:
+%            If WTS(Oj) equals TS(Ti):                   // if current value is what this transaction wrote to it
+%                O_j = oldO_j                            // restore old value
+%                WTS(O_j) = oldWTS(O_j)                  // restore old value
+% usage:  NewDatabase = rollback(Database, TS, OLD)
+rollback(Database, _, []) -> Database;
+rollback(Database, TS, [ {Idx,OldVal,_,OldWTS} |RestOLD]) ->
+    WTS = get_WTS(Database, Idx),
+    RTS = get_RTS(Database, Idx),
+    if
+        WTS == TS ->
+            NewDatabase = lists:keyreplace(Idx, 1, Database, {Idx, OldVal, RTS, OldWTS}),
+            rollback(NewDatabase, TS, RestOLD);
+        true ->
+            Database
+    end.
 
 get_WTS(DB, Idx) ->
     element(4, lists:keyfind(Idx, 1, DB)).
@@ -184,9 +212,15 @@ add_to_DEP(ClientList, TS, DependOnTS) ->
     {Client, _, DEP, OLD} = lists:keyfind(TS, 2, ClientList),
     lists:keyreplace(TS, 2, ClientList, {Client, TS, sets:add_element(DependOnTS, DEP), OLD}).
 
+% add OldObject to OLD list if there isn't an earlier copy already
 add_to_Old(ClientList, TS, OldObject) ->
     {Client, _, DEP, OLD} = lists:keyfind(TS, 2, ClientList),
-    lists:keyreplace(TS, 2, ClientList, {Client, TS, DEP, [OldObject|OLD]}).
+    case lists:keyfind(element(1,OldObject), 1, OLD) of
+        false ->
+            lists:keyreplace(TS, 2, ClientList, {Client, TS, DEP, [OldObject|OLD]});
+        _Other ->
+            ClientList
+    end.
 
 do_write(DB, Idx, Val, TS) ->
     lists:keyreplace(Idx, 1, DB, {Idx, Val, get_RTS(DB, Idx), TS}).
@@ -195,10 +229,11 @@ do_write(DB, Idx, Val, TS) ->
 announce_completion(Checking) ->
     lists:map(fun(CheckingPid) -> CheckingPid ! someone_committed end, Checking).
 
-%check_history(History, DEP) -> commit or abort or wait
-check_history([], _) -> io:format("our_error_empty_history~n");
-check_history(_, []) -> commit;
-check_history(History, [TS|Rest]) ->
+%check_history(Committer, History, DEP) -> commit or abort or wait
+check_history(_, [], _) -> io:format("our_error_empty_history~n");
+check_history(_, _, []) -> commit;
+check_history(Committer, History, [Committer|Rest]) -> check_history(Committer, History, Rest);  % skip self
+check_history(Committer, History, [TS|Rest]) ->
     {_, Status} = lists:keyfind(TS, 1, History),
     case Status of 
         aborted ->
@@ -206,7 +241,7 @@ check_history(History, [TS|Rest]) ->
         running ->
             wait;
         committed ->
-            check_history(History, Rest)
+            check_history(Committer, History, Rest)
     end.
 
 %handle_action({write, Idx, Val}, TS) ->
