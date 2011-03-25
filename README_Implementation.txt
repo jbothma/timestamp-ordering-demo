43c74bd7ae9881bc13c567eca46323fdff4eb4ca
JD Bothma <jbothma@gmail.com>
2011-03-25 12:52:19 +0000

    Store my readme in the right place. Improve commenting and readability in server.

--- README_Implementation.txt
+++ README_Implementation.txt
@@ -1,26 +1,63 @@
--------------------------------------------------------------------
-|
-| Course - Distributed Systems VT08
-| Project - Concurrency Control in Transaction
-|
-| Release - v 1.0 - 2 Mars 2008
-|
--------------------------------------------------------------------
+README for Distributed Systems VT11 Project
 
+Concurrency Control through Timestamp Ordering as described at 
+	http://en.wikipedia.org/w/index.php?title=Timestamp-based_concurrency_control&oldid=410754608
 
-1/ Files:
-The files provided in the archive are: parse.erl, client.erl, 
-window.erl, server.erl, README.txt, Makefile, system.jpg
+Modifications:
+	When action is 'write' and WTS > TS, we abort instead of applying Thomas Write Rule.
+	This is done because the rollback action of aborting a transaction can make
+	the application of the Thomas Write rule invalid.
 
-2/ Makefile:
- - In order to compile run make
- - In order to clen run make clean
-In case you add other files don't forget to add them in the Makefile
+Data:
+  ClientList is list of tuples like {Client,TS,DEP,OLD}
+  Client is client Pid
+  TS
+    Transaction timestamp generated from running transaction count each time a client sends 'request'.
+    This assumes each client only attempts one transaction at a time, which is 
+    consistent with the client implementation given.
+  DEP
+    int timestamp
+    Erlang Set of transactions that a given transaction depends on. Cleared when transaction ends.
+  OLD
+	List of old objects, stored when transaction writes a new value, to be able to roll back if
+    it must abort. Only stored first time, not replaced by later writes.
+	Database is list of tuples like {Idx, Val, RTS, WTS} where Idx is one of a..d and Val is stored int value.
+  RTS
+    int timestamp
+  WTS
+    int timestamp
+  TrnHist
+    Transaction history: a list of tuples {TS,Status} where TS is transaction timestamp
+    and Status is one of running, committed, aborted. Only added to in this implementation.
+    An improvement would be to check every now and then if there are any transactions that
+    no running transaction depends on, and delete them.
+Function:
+  Start transaction
+    Increment transaction count, 
+    assign value to client as new transaction timestamp, 
+    continue server loop with transaction count and ClientList
+  Action
+    Send database the action
+    wait for database to say it was ok or that transaction should abort.
+    update DEP or OLD depending on what action it was
+    continue server loop with new ClientList
+  Confirm
+    Spawn process to to check whether all depended-on transactions have completed
+      confirm if for each TS in Dep, the transaction status in TrnHist is committed.
+    continue server loop
+  Abort
+    send rollback and OLD to database
+    wait for rollback to complete
+    end transaction
+    continue server loop
 
-3/ OS:
-The following skeleton has been compiled and run under Mac OS X and 
-Linux. (Not tested under windows.)
--------------------------------------------------------------------
-<Noomene.BenHenda@it.uu.se>
-
+Satisfaction of properties:
+  Avoids deadlock
+    Committing a transaction blocks only the checking process while there are incomplete
+    transactions in the depends-on set DEP of the transaction, and by the
+    read-rule, transactions in DEP for Ti must have TS < Ti, thus deadlock is
+    avoided because no Tj in DEP(Ti) can be in DEP(Tj) (no cycles).
+  Recoverable
+    Doesn't commit if it read a value written by an earlier transaction which aborted:
+    
 
--- server.erl
+++ server.erl
@@ -20,13 +20,17 @@ start() ->
 
 initialize() ->
 	process_flag(trap_exit, true),
-	Initialvals = [{a,0,0,0},{b,0,0,0},{c,0,0,0},{d,0,0,0}], %% All variables are set to 0
-	ServerPid = self(),
-	StorePid = spawn_link(fun() -> store_loop(ServerPid,Initialvals) end),
-	% initial TrnCnt is 0
-	TrnHist = [{0, committed}], % hack for initial RTS and WTS
-	% initial Checking is []
-	server_loop([], StorePid, 0, TrnHist, []).
+    %% Initial values and RTS and WTS are 0
+	Initialvals = [{a,0,0,0},{b,0,0,0},{c,0,0,0},{d,0,0,0}], 
+	ServerPid   = self(),
+	StorePid    = spawn_link(fun() -> store_loop(ServerPid,Initialvals) end),
+
+    ClientList  = [],
+	TrnCnt      = 0,
+    % TrnHist starts with fake transaction 0 for comparisons against initial RTS and WTS
+	TrnHist     = [{0, committed}], 
+	Checking    = [],
+	server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking).
 %%%%%%%%%%%%%%%%%%%%%%% STARTING SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -35,35 +39,42 @@ initialize() ->
 %% the values of the global variable a, b, c and d 
 %% TrnCnt is Transaction Count, incremented in start_transaction
 server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking) ->
-	%io:format("Server: ClientList=~p~n", [ClientList]),
 	receive
-	{login, MM, Client} -> 
+	{login, MM, Client} ->  % client joining server
 		MM ! {ok, self()},
 		io:format("New client has joined the server:~p.~n", [Client]),
 		StorePid ! {print, self()},
 		server_loop(add_client(Client,ClientList), StorePid, TrnCnt, TrnHist, Checking);
-	{close, Client} -> 
+
+	{close, Client} ->      % client leaving server
 		io:format("Client ~p has left the server.~n", [Client]),
 		StorePid ! {print, self()},
 		server_loop(remove_client(Client,ClientList), StorePid, TrnCnt, TrnHist, Checking);
-	{request, Client} -> 
+
+	{request, Client} ->    % client initiating a transaction
 		TS = get_transaction(ClientList, Client),
 		if
 			is_integer(TS) ->
-				%TODO this is a hack to wait until previous transaction finished
-				self() ! {request, Client}, %re-queue request :D 
+				% re-queue client's request if it already has a transaction open:   
+                % Anything delaying completion of the transaction will eventually
+                % get through the queue and the next transaction can start.
+                % The current client waits for abort or confirm before starting a new
+                % transaction, but since this server implementation assumes this, it's
+                % good to enforce the assumption.
+				self() ! {request, Client},
 				server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking);
 			true ->
 				Client ! {proceed, self()},
-				io:format("request from ~p, starting new transaction.~n", [Client]),
+				io:format("Server: Request from client ~p, starting new transaction.~n", [Client]),
 				{NewTrnCnt, NewClientList} = start_transaction(TrnCnt, ClientList, Client),
 				NewTrnHist = [{NewTrnCnt, running}|TrnHist],
-				io:format("New transaction count stands at ~p~n", [NewTrnCnt]),
+				io:format("    New transaction count stands at ~p~n", [NewTrnCnt]),
 				server_loop(NewClientList, StorePid, NewTrnCnt, NewTrnHist, Checking)
 		end;
-	{confirm, Client} -> 
+
+	{confirm, Client} ->    % client asking to complete a transaction
 		ServerPid = self(),
-		io:format("confirm from ~p~n", [Client]),
+		io:format("Server: confirm from client ~p.~n", [Client]),
 		{_, TS, DEP, _} = lists:keyfind(Client, 1, ClientList),
 		if
 			is_integer(TS) ->
@@ -74,44 +85,59 @@ server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking) ->
 				io:format("Server: Client ~p sent confirm but has no transaction open.~n", [Client]),
 				server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking)
 		end;
-	{action, Client, Act} ->
-		%io:format("Received ~p from client ~p.~n", [Act, Client]),
+
+	{action, Client, Act} ->    % client sending one action in a transaction
 		TS = get_transaction(ClientList, Client),
 		if
 			is_integer(TS) ->
 				StorePid ! {Act, TS, self()},
-				receive
-					{read_ok, Idx, Val, WTS} ->  % do DEP(T_i).add(WTS(O_j))
-						io:format("Server: Trn ~p read ~p allowed: ~p.~n", [TS, Idx, Val]),
-						NewClientList = add_to_DEP(ClientList, TS, WTS);
-					{write_ok, Idx, OldObject} ->  % Let server do OLD(T_i).add( O_j, WTS(O_j) )				
-						io:format("Server: Trn ~p write ~p allowed.~n", [TS, Idx]),
-						NewClientList = add_to_Old(ClientList, TS, OldObject);
-					{abort, TS, Reason} ->
-						io:format("Server: DB said abort trn ~p. Reason: ~p.~n", [TS, Reason]),
-						{NewClientList, NewTrnHist, NewChecking} = 
-							abort(TS, -1, ClientList, StorePid, TrnHist,Checking, self()),
-						server_loop(NewClientList, StorePid, TrnCnt, NewTrnHist, NewChecking)
+				receive % block until DB responds with <op>_ok or abort
+
+				{read_ok, Idx, Val, WTS} ->  % do DEP(T_i).add(WTS(O_j))
+					io:format("Server: Trn ~p read ~p allowed: ~p.~n", [TS, Idx, Val]),
+					NewClientList = add_to_DEP(ClientList, TS, WTS);
+
+				{write_ok, Idx, OldObject} ->  % Let server do OLD(T_i).add( O_j, WTS(O_j) )				
+					io:format("Server: Trn ~p write ~p allowed.~n", [TS, Idx]),
+					NewClientList = add_to_OLD(ClientList, TS, OldObject);
+
+				{abort, TS, Reason} ->
+					io:format("Server: DB said abort trn ~p. Reason: ~p.~n", [TS, Reason]),
+					{NewClientList, NewTrnHist, NewChecking} = 
+						abort(TS, -1, ClientList, StorePid, TrnHist,Checking, self()),
+					server_loop(NewClientList, StorePid, TrnCnt, NewTrnHist, NewChecking)
 				end,
 				server_loop(NewClientList, StorePid, TrnCnt, TrnHist, Checking);
 			true -> %TS isn't an int, it's probably nil which means no transaction for this client.
 				io:format("Server: Client ~p sent action ~p but has no transaction open.~n", [Client, Act]),
 				server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking)
 		end;
-	{abort, TS, Reason, ChkPid} ->
+
+	{abort, TS, Reason, ChkPid} ->  % comfirm check process rejecting transaction
 		io:format("Server: check trn ~p said ABORT. Reason: ~p.~n", [TS,Reason]),
 		{NewClientList, NewTrnHist, NewChecking} = 
 			abort(TS, ChkPid, ClientList, StorePid, TrnHist,Checking, self()),
 		server_loop(NewClientList, StorePid, TrnCnt, NewTrnHist, NewChecking);
-	{commit, TS, ChkPid} ->
+
+	{commit, TS, ChkPid} ->         % confirm check process approving transaction
+		io:format("Server: check trn ~p said COMMIT.~n", [TS]),
+
 		% set status in history to committed
-		NewTrnHist  = lists:keyreplace(TS, 1, TrnHist, {TS, committed}),
-		NewChecking = Checking--[ChkPid],
-		Client	  = element(1, lists:keyfind(TS, 2, ClientList)),
-		NewClientList = end_transaction(ClientList, Client),
+		NewTrnHist      = lists:keyreplace(TS, 1, TrnHist, {TS, committed}),
+
+        % The commit check proc finished upon commit. It shouldn't get commit 
+        % notifications any more, so remove from Checking list.
+		NewChecking     = Checking--[ChkPid],
+
+		Client          = element(1, lists:keyfind(TS, 2, ClientList)),
+		NewClientList   = end_transaction(ClientList, Client),
+
+        % inform client
 		Client ! {committed, self()},
+
 		announce_completion(Checking),
 		server_loop(NewClientList, StorePid, TrnCnt, NewTrnHist, NewChecking)
+
 	after 50000 ->
 	case all_gone(ClientList) of
 		true -> exit(normal);	
@@ -119,98 +145,112 @@ server_loop(ClientList, StorePid, TrnCnt, TrnHist, Checking) ->
 	end
 	end.
 
+
 %% - The values are maintained here
 store_loop(ServerPid, Database) -> 
 	receive
 	{print, ServerPid} -> 
-		io:format("Database status:~n~p.~n",[Database]),
+		io:format("DB status: ~p.~n",[Database]),
 		store_loop(ServerPid,Database);
-	{{write, Idx, Val}, TS, ServerPid} ->
+
+	{{write, Idx, Val}, TS, ServerPid} ->   % action from client matched as WRITE
 		io:format("DB: Must write val ~p to idx ~p for trn ~p~n", [Val, Idx, TS]),
 		WTS = get_WTS(Database, Idx),
 		RTS = get_RTS(Database, Idx),
 		if
-			%http://en.wikipedia.org/wiki/Talk:Timestamp-based_concurrency_control#Thomas_Write_Rule
+            % abort if written by later transaction instead of applying "ignore obsolete write"
+            % because rollback of a later transaction can make application of this rule invalid.
 			(WTS > TS) or (RTS > TS) ->
 				Reason = "WTS " ++ integer_to_list(WTS) ++  " > TS " ++ integer_to_list(TS) ++
 					" or RTS " ++ integer_to_list(RTS) ++ " > TS " ++ integer_to_list(TS),
 				ServerPid ! {abort, TS, Reason},
 				NewDatabase = Database;
-			true ->
+			true -> % else: write is allowed
 				OldObject = lists:keyfind(Idx, 1, Database),
-				%set WTS(O_j) = TS(T_i)
-				%do write
+				%do the write and set WTS(O_j) = TS(T_i)
 				NewDatabase = do_write(Database, Idx, Val, TS),
 				ServerPid ! {write_ok, Idx, OldObject}  % Let server do OLD(T_i).add( O_j, WTS(O_j) )
 		end,
-		io:format("Database status:~n~p.~n",[NewDatabase]),
+		io:format("DB status: ~p.~n",[NewDatabase]),
 		store_loop(ServerPid,NewDatabase);
-	{{read, Idx}, TS, ServerPid} ->
+
+	{{read, Idx}, TS, ServerPid} ->     % action from client matched as READ
 		io:format("DB: Must read idx ~p for trn ~p~n", [Idx, TS]),
-%		WTS = get_WTS(Database, Idx),
 		{_, Val, RTS, WTS} = lists:keyfind(Idx, 1, Database),
 		if
 			WTS > TS ->
-				Reason = "WTS" ++ integer_to_list(WTS) ++ "TS" ++ integer_to_list(TS),
+				Reason = "WTS " ++ integer_to_list(WTS) ++ " > TS " ++ integer_to_list(TS),
 				ServerPid ! {abort, TS, Reason},
 				NewDatabase = Database;
-			true ->				 
-				ServerPid ! {read_ok, Idx, Val, WTS},	 % Let server do DEP(T_i).add(WTS(O_j))
-%				RTS = get_RTS(Database, Idx),
-				NewDatabase = set_RTS(Database, Idx, erlang:max(RTS, TS))
+			true ->	  % else: read is allowed
+				NewDatabase = set_RTS(Database, Idx, erlang:max(RTS, TS)),
+				ServerPid ! {read_ok, Idx, Val, WTS}	 % Let server do DEP(T_i).add(WTS(O_j))
 		end,
-		io:format("Database status:~n~p.~n",[NewDatabase]),
+		io:format("DB status: ~p.~n",[NewDatabase]),
 		store_loop(ServerPid, NewDatabase);
-	{rollback, TS, OLD, ServerPid} ->
+
+	{rollback, TS, OLD, ServerPid} ->   % server said to rollback writes by trn TS with values in OLD
 		io:format("DB: rolling back OLD=~p~n", [OLD]),
 		NewDatabase = rollback(Database, TS, OLD),
-		ServerPid ! rollback_complete,  % TODO server blocks until this is done but not sure it's necesary
-		io:format("Database status:~n~p.~n",[NewDatabase]),
+		io:format("DB status: ~p.~n",[NewDatabase]),
+		ServerPid ! rollback_complete,
 		store_loop(ServerPid, NewDatabase)
 	end.
 
-% spawned as separate process from server_loop.
-% each loop, check status of each transaction DEP.
+
+% Process checking whether transaction can complete,
+% spawned from server_loop.
+%
+% Each loop, check status of each transaction DEP.
+%
 % Wait for a 'someone_completed' from server_loop each 
 % time a running transaction is encountered in DEP.
-% restart loop with up-to-date history upon 'someone_completed'
+%
+% Restart loop with more recent history upon 'someone_completed' msg from server.
 check_commit_loop(TS, DEP, TrnHist, ServerPid) ->
 	%io:format("checking trn ~p: DEP=~p   TrnHist=~p~n", [TS, sets:to_list(DEP), TrnHist]),
 	CommitCheck = check_history(TS, TrnHist, sets:to_list(DEP)),
 	case CommitCheck of
-		{wait, Reason} ->
-			io:format("checking: wait...~p.~n", [Reason]),
-			receive 
-			{someone_completed, NewTrnHist} ->
-				check_commit_loop(TS, DEP, NewTrnHist, ServerPid)
-			end;
-		{abort, Reason} ->
-			%io:format("checking: abort~n"),
-			ServerPid ! {abort, TS, Reason, self()};
-		commit ->
-			%io:format("checking: commit~n"),
-			ServerPid ! {commit, TS, self()}
+	{wait, Reason} ->
+		io:format("checking: wait...~p.~n", [Reason]),
+		receive 
+		{someone_completed, NewTrnHist} ->
+			check_commit_loop(TS, DEP, NewTrnHist, ServerPid)
+		end;
+	{abort, Reason} ->
+		%io:format("checking: abort~n"),
+		ServerPid ! {abort, TS, Reason, self()};
+	commit ->
+		%io:format("checking: commit~n"),
+		ServerPid ! {commit, TS, self()}
 	end.
-
 %%%%%%%%%%%%%%%%%%%%%%% ACTIVE SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 abort(TS, ChkPid, ClientList, StorePid, TrnHist, Checking, ServerPid)  ->
 	{Client, _, _, OLD} = lists:keyfind(TS, 2, ClientList),
 	StorePid ! {rollback, TS, OLD, ServerPid},
-	%set status in history to aborted
+
+	% set status in history to aborted
 	NewTrnHist	  = lists:keyreplace(TS, 1, TrnHist, {TS, aborted}),
+    
+    % checking proc will have ended, stop sending completion messages
 	NewChecking	 = Checking--[ChkPid],
+
 	NewClientList   = end_transaction(ClientList, Client),
+
+    % inform client that trn was aborted
 	Client ! {abort, self()},
+
 	announce_completion(Checking),
 	{NewClientList, NewTrnHist, NewChecking}.
 
 
 
-%		for each (oldO_j, oldWTS(O_j)) in OLD(T_i):	 // make sure nothing persists from this aborting transaction:
-%			If WTS(Oj) equals TS(Ti):				   // if current value is what this transaction wrote to it
-%				O_j = oldO_j							// restore old value
-%				WTS(O_j) = oldWTS(O_j)				  // restore old value
+%for each (oldO_j, oldWTS(O_j)) in OLD(T_i):    // make sure nothing persists from this aborting transaction:
+%   If WTS(Oj) equals TS(Ti):                   // if current value is what this transaction wrote to it
+%       O_j = oldO_j                            // restore old value
+%       WTS(O_j) = oldWTS(O_j)                  // restore old value
 % usage:  NewDatabase = rollback(Database, TS, OLD)
 rollback(Database, _, []) -> Database;
 rollback(Database, TS, [ {Idx,OldVal,_,OldWTS} |RestOLD]) ->
@@ -224,12 +264,10 @@ rollback(Database, TS, [ {Idx,OldVal,_,OldWTS} |RestOLD]) ->
 			Database
 	end.
 
+
+
 get_WTS(DB, Idx) ->
 	element(4, lists:keyfind(Idx, 1, DB)).
-
-%set_WTS(DB, Idx, NewWTS) ->
-%	{_, Val, RTS, _} = lists:keyfind(Idx, 1, DB),
-%	lists:keyreplace(Idx, 1, DB, {Idx, Val, RTS, NewWTS}).
 
 get_RTS(DB, Idx) ->
 	element(3, lists:keyfind(Idx, 1, DB)).
@@ -242,8 +280,9 @@ add_to_DEP(ClientList, TS, DependOnTS) ->
 	{Client, _, DEP, OLD} = lists:keyfind(TS, 2, ClientList),
 	lists:keyreplace(TS, 2, ClientList, {Client, TS, sets:add_element(DependOnTS, DEP), OLD}).
 
+
 % add OldObject to OLD list if there isn't an earlier copy already
-add_to_Old(ClientList, TS, OldObject) ->
+add_to_OLD(ClientList, TS, OldObject) ->
 	{Client, _, DEP, OLD} = lists:keyfind(TS, 2, ClientList),
 	case lists:keyfind(element(1,OldObject), 1, OLD) of
 		false ->
@@ -252,17 +291,21 @@ add_to_Old(ClientList, TS, OldObject) ->
 			ClientList
 	end.
 
+% update value and WTS
 do_write(DB, Idx, Val, TS) ->
 	lists:keyreplace(Idx, 1, DB, {Idx, Val, get_RTS(DB, Idx), TS}).
+
 
 % send all Checking transactions a "someone_completed" with updated transaction history
 announce_completion(Checking) ->
 	lists:map(fun(CheckingPid) -> CheckingPid ! someone_committed end, Checking).
 
-%check_history(Committer, History, DEP) -> commit or abort or wait
+
+%Usage: check_history(Committer, History, DEP) -> commit OR abort OR wait
 check_history(_, [], _) -> io:format("our_error_empty_history~n");
 check_history(_, _, []) -> commit;
-check_history(Committer, History, [Committer|Rest]) -> check_history(Committer, History, Rest);  % skip self
+check_history(Committer, History, [Committer|Rest]) -> 
+    check_history(Committer, History, Rest);  % skip self
 check_history(Committer, History, [TS|Rest]) ->
 	{_, Status} = lists:keyfind(TS, 1, History),
 	case Status of 
@@ -273,16 +316,9 @@ check_history(Committer, History, [TS|Rest]) ->
 		committed ->
 			check_history(Committer, History, Rest)
 	end.
-
-%handle_action({write, Idx, Val}, TS) ->
-%	io:format("Must write val ~p to idx ~p for client ~p~n", [Val, Idx, TS]);
-%handle_action({read, Idx}, TS) ->
-%	io:format("Must read idx ~p for client ~p~n", [Idx, TS]).
 	
 
-% Return new client list where the client tuple has been replaced with
-% one where all values are the same except for the new transaction number.
-% !!!NOTE!!! it assumes each client only ever attempts one transaction at a time!
+% new transaction count and client list with client's transaction timestamp/ID updated
 start_transaction(TrnCnt, ClientList, Client) ->
 	NewTrnCnt		   = TrnCnt + 1,
 	{_, _, DEP, OLD}	= lists:keyfind(Client, 1, ClientList),
@@ -294,18 +330,17 @@ end_transaction(ClientList, Client) ->
 get_transaction(ClientList, Client) -> 
 	element(2, lists:keyfind(Client, 1, ClientList)).
 
-%% - Low level function to handle lists
-add_client(C,ClientList) -> [new_client(C)|ClientList].
 
 new_client(C) -> {C, nil, sets:new(),[]}.
 
+%% - Low level function to handle lists
+add_client(C,ClientList) -> [new_client(C)|ClientList].
+
 % remove anything from empty list returns empty list
 remove_client(_,[]) -> [];
-% if client is first, return rest
 remove_client(C, [ {C,_,_,_} | T ]) -> T;
-% falling through when client wasn't first, 
-% recurse down and return first and rest with client removed from in-between
 remove_client(C, [H|T]) -> [H|remove_client(C,T)].
+
 
 all_gone([]) -> true;
 all_gone(_) -> false.
